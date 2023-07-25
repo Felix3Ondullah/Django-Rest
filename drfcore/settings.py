b8046f9b91559d1b5ab41fefabd5523d813d9a19
+ <+>
2023-07-25 15:24:49 +0300

    worked on the db relations with docker

--- drfcore/settings.py
+++ drfcore/settings.py
@@ -69,10 +69,22 @@ WSGI_APPLICATION = 'drfcore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+#         "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+#         "USER": os.environ.get("SQL_USER", "ondullah"),
+#         "PASSWORD": os.environ.get("SQL_PASSWORD", "Berger"),
+#         "HOST": os.environ.get("SQL_HOST", "localhost"),
+#         "PORT": os.environ.get("SQL_PORT", "5432"),
+#     }
+# }
+
+
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.postgresql_psycopg2"),
+        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "postgresql_psycopg2"),
         "USER": os.environ.get("SQL_USER", "ondullah"),
         "PASSWORD": os.environ.get("SQL_PASSWORD", "Berger"),
         "HOST": os.environ.get("SQL_HOST", "localhost"),
