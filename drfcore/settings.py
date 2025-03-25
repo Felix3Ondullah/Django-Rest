59e476952843f17ffd2876276a380f73c51bfd63
Felix <felixondullah@gmail.com>
2025-03-25 15:00:21 +0300

    new

--- drfcore/settings.py
+++ drfcore/settings.py
@@ -10,6 +10,12 @@ SECRET_KEY = "your-secret-key-here"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+import os
+
+STATIC_URL = "/static/"
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]  # Make sure this folder exists
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Collects all static files here
+
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
@@ -24,11 +30,14 @@ INSTALLED_APPS = [
     # My apps
     'rest_api',
        'jazzmin',
+  "widget_tweaks",    # Tailwind CSS integration
+       
        
     # Third-party apps
     'rest_framework',
     'rest_framework.authtoken',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -42,10 +51,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'drfcore.urls'
 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "rest_api/templates")],  # Custom template directory
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -57,6 +67,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'drfcore.wsgi.application'
 
@@ -86,12 +97,40 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
+
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+JAZZMIN_SETTINGS = {
+    "site_title": "My Custom Admin",
+    "site_header": "My Dashboard",
+    "welcome_sign": "Welcome to My Admin Panel",
+    "show_ui_builder": True,  # Enables UI customizer
+
+    # **Theme Customization**
+    "theme": "litera",  # Try other Bootstrap themes like 'darkly', 'lux', 'materia', etc.
+    "site_logo": "static/images/logo.png",  # Add your logo
+    "site_icon": "static/images/favicon.ico",  # Favicon
+    "login_logo": "static/images/login-logo.png",
+    
+    # **Top Menu Customization**
+    "topmenu_links": [
+        {"name": "Home", "url": "admin:index", "permissions": ["auth.view_user"]},
+        {"model": "auth.User"},
+        {"model": "rest_api.Customer"},
+    ],
+
+    # **Side Menu Customization**
+    "usermenu_links": [
+        {"name": "Support", "url": "https://support.example.com", "new_window": True},
+        {"model": "auth.user"},
+    ],
+
+    # **Custom Footer**
+    "copyright": "© 2025 My Company",
+}
 
 
 
